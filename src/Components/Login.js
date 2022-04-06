ff2e27a6142690f94baf50797ba8be686c28754d
Mutawally Nawwar <89324014+Myumet@users.noreply.github.com>
2022-04-06 17:55:15 +0700

    Update Login.js

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -51,6 +51,7 @@ export default function Login() {
         <div className='containerAuth'>
             <div className='bagianKiriAuth'>
                 <img src={require('../Images/vektor.jpg')}/>
+                <a href='https://www.freepik.com/vectors/social-media-network' style={{fontSize: '12px'}}>Social media network vector created by storyset - www.freepik.com</a>
             </div>
             <div className='bagianKananAuth'>
                  <div className='logoAuth'>
